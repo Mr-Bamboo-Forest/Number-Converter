9240a1472ff04c7825906677b25d25a5ad4823e2
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2023-02-28 20:48:27 +1000

    fix

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //Square Number Calculate
 function squarecalc(){
-    var squaresolveanswer = Number(document.getElementById("squaresolve").value * Number(document.getElementById("squaresolve").value));
-    document.getElementById("squareanswer").innerText = comma(squaresolveanswer);  
-}
+    var select = document.querySelector('#units');
+    var output = select.value;
+    console.log(output)  
+};
+function comma(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
